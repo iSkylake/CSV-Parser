3967e5b203b21204e096cd657ea4a4656880cbaa
Llorch <SkylakeBeta@gmail.com>
2017-03-22 22:58:08 -0700

    Ignores commas in double quotes

--- csvparser.js
+++ csvparser.js
@@ -6,7 +6,39 @@ fs.readFile('data.csv', function(err, csvData){
 	} else {
 		var arr = csvData.toString().split('\r\n');
 
-		var header = arr[0].split(', ');
+		console.log(arr[0]);
+		var header = [];
+
+		var first = 0;
+		var last = 0;
+		var index = 0;
+
+		while(index < arr[0].length){
+			if(arr[0][index] === '"'){
+				first = index;
+				index++;
+				while(arr[0][index] !== '"'){
+					index++;
+				}
+				index++;
+			}
+			if(arr[0][index] === ',' || index === arr[0].length-1){
+				if(index === arr[0].length-1){
+					last = index + 1;
+				} else{
+					last = index;
+				}
+				header.push(arr[0].substring(first, last));
+				index = index + 2;
+				first = index;
+			} else{
+				index++;
+			}
+		}
+
+		console.log(header);
+
+		// var header = arr[0].split(', ');
 		var jsonData = [];
 
 		for(i=1; i<arr.length; i++){
