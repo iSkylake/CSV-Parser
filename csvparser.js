645360887d58d545b5a834719aecf7ce54c58ef9
Llorch <SkylakeBeta@gmail.com>
2017-03-23 17:05:01 -0700

    Check all double quotes

--- csvparser.js
+++ csvparser.js
@@ -4,50 +4,62 @@ fs.readFile('data.csv', function(err, csvData){
 	if(err){
 		console.log(err);
 	} else {
-		var arr = csvData.toString().split('\r\n');
+		var arr = csvData.toString().split('\r\n'); // '\r\n' is for Windows files, for Linux use '\n' and for Mac use '\r'
 
-		console.log(arr[0]);
+		// console.log(arr[0]);
 		var header = [];
+		var infoArr = [];
 
+		for(i=0; i<arr.length; i++){
 		var first = 0;
 		var last = 0;
 		var index = 0;
+		var tempArr = [];
 
-		while(index < arr[0].length){
-			if(arr[0][index] === '"'){
-				first = index;
-				index++;
-				while(arr[0][index] !== '"'){
+			while(index < arr[i].length){
+				if(arr[i][index] === '"'){
+					first = index;
+					index++;
+					while(arr[i][index] !== '"'){
+						index++;
+					}
 					index++;
 				}
-				index++;
-			}
-			if(arr[0][index] === ',' || index === arr[0].length-1){
-				if(index === arr[0].length-1){
-					last = index + 1;
+				if(arr[i][index] === ',' || index === arr[i].length-1){
+					if(index === arr[i].length-1){
+						last = index + 1;
+					} else{
+						last = index;
+					}
+					if(i === 0){
+						header.push(arr[i].substring(first, last));	
+					} else {
+						tempArr.push(arr[i].substring(first, last));
+					}			
+					index = index + 2;
+					first = index;
 				} else{
-					last = index;
+					index++;
 				}
-				header.push(arr[0].substring(first, last));
-				index = index + 2;
-				first = index;
-			} else{
-				index++;
+			}
+			if(tempArr.length > 0){
+				infoArr.push(tempArr);
 			}
 		}
 
-		console.log(header);
+		console.log(infoArr);
 
 		// var header = arr[0].split(', ');
-		var jsonData = [];
+		// var jsonData = [];
 
-		for(i=1; i<arr.length; i++){
-			var obj = {};
-			var data = arr[i].split(', ');
-			for(j=0; j<data.length; j++){
-				obj[header[j]] = data[j];
-			}
-			jsonData.push(obj);
-		}
+		// for(i=1; i<arr.length; i++){
+		// 	var obj = {};
+		// 	var data = arr[i].split(', ');
+		// 	for(j=0; j<data.length; j++){
+		// 		obj[header[j]] = data[j];
+		// 	}
+		// 	jsonData.push(obj);
+		// }
+		// console.log(jsonData);
 	}
 });
